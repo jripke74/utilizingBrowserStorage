7b8ffddcbec737bc97fefd26ba61262af6959f87
jripke74 <jripke74@gmail.com>
2020-06-19 09:46:44 -0700

    add indexDB

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
+const dbRequest = indexedDB.open('StorageDummy', 1);
 
-storeBtn.addEventListener('click', () => {
-  const userId = 'u123';
-  const user = {name: 'Jeff', age: 33};
-  document.cookie = `uid=${userId}; max-age=2`;
-  document.cookie = `user=${JSON.stringify(user)}`;
-});
+dbRequest.onupgradeneeded = function (event) {
+  const db = event.target.result;
 
-retrBtn.addEventListener('click', () => {
-  console.log(document.cookie)
-  const cookieData = document.cookie.split(';');
-  const data = cookieData.map(i => {
-    return i.trim();
-  });
-  console.log(data[1].split('=')[1]);
-});
+  const objStore = db.createObjectStore('products', { keyPath: 'id' });
+
+  objStore.transaction.oncomplete = function (event) {
+    const productsStore = db
+      .transaction('products', 'readwrite')
+      .objectStore('products');
+    productsStore.add({
+      id: 'p1',
+      title: 'A First Product',
+      price: 12.99,
+      tags: ['Expensive', 'Luxury']
+    });
+  };
+};
+
+dbRequest.onerror = function (event) {
+  console.log('ERROR!');
+};
+
+storeBtn.addEventListener('click', () => {});
+
+retrBtn.addEventListener('click', () => {});
